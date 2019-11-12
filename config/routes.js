8b7d64366d5b4791570bd07a3dd2fc5fe6a70c05
ionpascaru <iionpascaru@gmail.com>
2019-11-12 21:03:01 +0100

    celebrities done

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,11 @@ const controller = require('../controllers/base.controller');
 
 router.get('/', controller.base);
 router.get('/celebrities', controller.listCelebrities);
+router.get('/celebrities/add', controller.addCelebrity);
+router.post('/celebrities/add', controller.doAddCelebrity);
 router.get('/celebrities/:id', controller.celebrityDetail);
+router.get('/celebrities/:id/edit', controller.edit);
+router.post('/celebrities/:id/edit', controller.doEdit);
+router.post('/celebrities/:id/delete', controller.delete);
 
 module.exports = router;
--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const mongoose = require('mongoose');
 const Celebrity = require('../models/celebrity');
+const Movie = require('../models/movie');
 
 module.exports.base = (req, res, next) => {
     res.render('index', {
@@ -19,6 +20,21 @@ module.exports.listCelebrities = (req, res, next) => {
         );
 };
 
+module.exports.addCelebrity = (req, res, next) => {
+    res.render('celebrities/form', {
+        celebrity: new Celebrity()
+    })
+}
+
+module.exports.doAddCelebrity = (req, res, next) => {
+    console.info('body request => ', req.body)
+    const celebrity = new Celebrity(req.body)
+
+    celebrity.save()
+        .then(() => res.redirect('/celebrities'))
+        .catch(error => next(error));
+}
+
 module.exports.celebrityDetail = (req, res, next) => {
     const id = req.params.id;
 
@@ -35,3 +51,52 @@ module.exports.celebrityDetail = (req, res, next) => {
             );
     }
 };
+
+module.exports.edit = (req, res, next) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        next(createError(404));
+    } else {
+        Celebrity.findById(id)
+            .then(
+                celebrity => {
+                    res.render('celebrities/form', { celebrity })
+                }
+            ).catch(
+                error => next(error)
+            );
+    }
+}
+
+module.exports.doEdit = (req, res, next) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        next(createError(404));
+    } else {
+        Celebrity.findByIdAndUpdate(id, req.body, { new: true })
+            .then(celebrity => {
+                console.log(celebrity)
+                res.redirect('/celebrities')
+            })
+            .catch(
+                error => next(error)
+            )
+    }
+}
+
+module.exports.delete = (req, res, next) => {
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        next(createError(404));
+    } else {
+        Celebrity.findByIdAndDelete(id)
+            .then(celebrityDeleted => {
+                console.log('Celebrity deleted => ', celebrityDeleted)
+                res.redirect('/celebrities')
+            })
+            .catch(error => next(error))
+    }
+}
